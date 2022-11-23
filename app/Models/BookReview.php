5f97caa36d1f84ddbe80f3020a122452bceeb1b5
takahito-kinouchi <103920098+Takahito-Kinouchi@users.noreply.github.com>
2022-11-24 00:53:16 +0900

    テーブルの内容を編集可能に

--- app/Models/BookReview.php
+++ app/Models/BookReview.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class BookReview extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'review_title',
+        'user_id',
+        'book_title',
+        'author',
+        'publisher',
+        'content',
+    ];
 }
