c731d24e51e76e9f76b89945ea4655937c60ebf1
takahito-kinouchi <103920098+Takahito-Kinouchi@users.noreply.github.com>
2022-11-23 16:20:38 +0900

    書評のデータベースのセット

---
+++ database/factories/BookReviewFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class BookReviewFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $user = User::all();
+        return [
+            'user_id' => $user->random()->id,
+            'book_title' => $this->faker->word(),
+            'author' => $this->faker->name(),
+            'publisher' => $this->faker->company(),
+            'review_title' => $this->faker->sentence(),
+            'content' => $this->faker->paragraph(5),
+            'vote' => $this->faker->randomNumber(),
+        ];
+    }
+}
---
+++ database/migrations/2022_11_23_034246_book_review.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('book_title');
+            $table->string('author');
+            $table->string('publisher');
+            $table->string('review_title');
+            $table->longText('content');
+            $table->integer('vote');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_review');
+    }
+};
