486f2c1785c0b16b6ce402dbbb39e796de44a4c4
takahito-kinouchi <103920098+Takahito-Kinouchi@users.noreply.github.com>
2022-11-24 00:52:59 +0900

    書評のバリデーション

---
+++ app/Http/Requests/ReviewStoreRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ReviewStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        if ($this->path() == 'reviews') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'review_title' => ['required', 'min:6'],
+            'book_title' => ['required', 'min:6'],
+            'author' => ['required', 'min:6'],
+            'publisher' => ['required', 'min:6'],
+            'content' => ['required', 'min:50'],
+        ];
+    }
+}
