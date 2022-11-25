3e07883cc871def123e1cbee8ee2a645b25b8046
takahito-kinouchi <103920098+Takahito-Kinouchi@users.noreply.github.com>
2022-11-25 15:59:52 +0900

    upvote/downvote 処理の作成

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -7,29 +7,35 @@ use App\Models\Vote;
 
 class VoteController extends Controller
 {
-    public function upvote(Request $request){
-        dd($request);
-        $vote = array(
-            "review_id" => $request->review_id,
-            "user_id" => auth()->id(),
-            "vote" => 1,
+    public function vote(Request $request){
+        $review_id = $request->review_id;
+        $user_id = auth()->id();
+
+        switch ($request->path()){
+            case 'votes/upvote':
+                $vote = 1;
+                break;
+            case 'votes/downvote':
+                $vote = -1;
+                break;
+        }
+
+        //すでに評価済みのときはそれを削除
+        $previous_vote = Vote::where('review_id', $review_id)
+            ->where('user_id', $user_id);
+        if($previous_vote->count() > 0){
+            Vote::destroy($previous_vote->get());
+        }
+        $review_vote = array(
+            "review_id" => $review_id,
+            "user_id" => $user_id,
+            "vote" => $vote,
         );
 
         //新規投票作成
-        Vote::create($vote);
+        Vote::create($review_vote);
 
         return back()->with('message', '書評を投稿しました！');
     }
-
-    // public function downvote(VoteStoreRequest $request){
-    //     //この時点でバリデーション済み
-    //     $vote = $request->validated();
-    //     $vote['vote'] = 1;
-
-    //     //新規投票作成
-    //     Vote::create($vote);
-
-    //     return back()->with('message', '書評を投稿しました！');
-    // }
 }
 
